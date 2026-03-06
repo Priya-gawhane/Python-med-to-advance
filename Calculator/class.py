e21dc66abf9d302dcc148843fdfab68622b1ddf3
Priya-gawhane <priyagawhane75@gmail.com>
2026-03-06 23:51:44 +0530

    calculator using class

--- Calculator/class.py
+++ Calculator/class.py
@@ -1,1 +1,23 @@
 #building a calculator using class function
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Denominator cannot be zero")
+        return a / b
+    
+calc = Calculator()
+
+num1 = float(input("Enter 1st no:"))
+num2 = float(input("Enter 2nd no:"))
+
+print("Addition:", calc.add(num1, num2))
+print("Subtraction:", calc.subtract(num1, num2))
